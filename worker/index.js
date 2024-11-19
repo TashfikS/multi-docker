d4705225b6ecc118a847b5c40ae58ee5aa7968c9
TashfikS <tasfiksalehin10@gmail.com>
2024-11-19 10:54:29 +0600

    To support Redis v4+, React Hooks, and React Router v6+

--- worker/index.js
+++ worker/index.js
@@ -2,20 +2,22 @@ const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+    url: `redis://${keys.redisHost}:${keys.redisPort}`,
+    retry_strategy: () => 1000,
 });
 
 const sub = redisClient.duplicate();
 
-function fib(index){
+function fib(index) {
     if (index < 2) return 1;
-    return fib(index-1) + fib(index-2);
+    return fib(index - 1) + fib(index - 2);
 }
 
-sub.on('message', (channel, message)=>{
-    redisClient.hset('values', message, fib(parseInt(message)));
-});
+(async () => {
+    await redisClient.connect();
+    await sub.connect();
 
-sub.subscribe('insert');
+    sub.subscribe('insert', (message) => {
+        redisClient.hSet('values', message, fib(parseInt(message)));
+    });
+})();
